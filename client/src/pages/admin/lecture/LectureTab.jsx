07e08103ec2f60bd44eb04ffd8a1129d7c9703b7
Ekansh <ekansh54v@gmail.com>
2025-04-11 11:11:35 +0530

    Fix Lecture tab

--- client/src/pages/admin/lecture/LectureTab.jsx
+++ client/src/pages/admin/lecture/LectureTab.jsx
@@ -11,7 +11,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { toast } from 'sonner'
 
-const MEDIA_API = "http://localhost:8080/api/v1/media";
+const MEDIA_API = "https://lms-hhw0.onrender.com/api/v1/media";
 
 const LectureTab = () => {
 
